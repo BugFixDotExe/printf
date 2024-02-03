4970f0ab4d20b17cd1e81270a58fafb04b26fe0d
BugFixDotExe <bugfix.exe@gmail.com>
2024-02-03 06:43:23 -0800

    trying to solve for  %x

---
+++ int_to_hex_handle.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int_to_hex - a function that converts
+ * an int to the hex equivalent
+ * @data: the int to be converted
+ * @buffer: the buffer to write to
+ * Return: the count of characters in buffer
+ */
+int int_to_hex(int data, void *buffer)
+{
+	int count;
+
+	count = 0;
+	count = sprintf(buffer, "%x", data);
+	count += write(1, buffer, count);
+	return (count);
+}
